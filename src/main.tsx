2a8411090fa58d0ecfbf404e17ae55d151bd1647
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-08-05 18:29:56 +0500

    login for located center

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { client } from './config/query-client.ts'
+import 'sweetalert2/src/sweetalert2.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={client}>
